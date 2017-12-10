2446aea0ee09e0024f3976daba8322c16465a291
André Michel Andy Pozos <andreandyp@outlook.com>
2017-12-10 14:24:20 -0600

    Animación de DyV incompleta

--- js/algoritmos.js
+++ js/algoritmos.js
@@ -1,8 +1,10 @@
 "use strict";
 
+var nivel = 0;
 $(document).ready(function(){
 	var aceOpciones = {
-		maxLines: Infinity
+		maxLines: Infinity,
+		readOnly: true
 	};
 
 	var editNormal = ace.edit("editNormal");
@@ -14,8 +16,8 @@ $(document).ready(function(){
 	var editOptima = ace.edit("editOptima");
 	editOptima.setTheme("ace/theme/iplastic");
 	editOptima.getSession().setMode("ace/mode/javascript");
-	editNormal.setOptions(aceOpciones);
-	editNormal.renderer.$cursorLayer.element.style.display = "none"
+	editOptima.setOptions(aceOpciones);
+	editOptima.renderer.$cursorLayer.element.style.display = "none";
 
 	var grande = 1000;
 
@@ -44,19 +46,18 @@ $(document).ready(function(){
 			nums.push(num);
 		}
 
-		var resDV = divideMSS(nums, 0, nums.length - 1);
-		$("#resDV").text("Resultado (optimizada): "+resDV);
-		alert("Terminó la forma rápida");
-		
+		divideMSS(nums, 0, nums.length - 1, false).then(function(resDV){
+			$("#resDV").text("Resultado (optimizada): "+resDV);
+		});
+		alert("Terminó la forma óptima");
+
 		fuerzaBrutaMSS(nums, nums.length, false).then(function(resFB){
 			$("#resFB").text("Resultado (normal): "+resFB);
 		});
-		alert("Terminó la forma lenta");
-		
+		alert("Terminó la forma normal");
 	});
 
-
-	$("#resolver").click(function(){
+	$("#resolverN").click(function(){
 		var nums = $("#nums").val();
 		nums = nums.split(" ");
 		for(var i = 0; i < nums.length; i++){
@@ -64,13 +65,27 @@ $(document).ready(function(){
 		}
 
 		$(".animado").empty();
-		crearAnimDV(nums);
-
-		var resDV = divideMSS(nums, 0, nums.length - 1);
-		$("#res").text("Resultado: "+resDV);
-
+		$("#collapsibleNormal").collapsible("open", 0);
 		crearAnimFB(nums);
-		var resFB = fuerzaBrutaMSS(nums, nums.length, true, editNormal);
+		fuerzaBrutaMSS(nums, nums.length, true, editNormal).then(function(resFB){
+			$("#res").text("Resultado: "+resFB);
+		});
+	});
+
+	$("#resolverO").click(function(){
+		var nums = $("#nums").val();
+		nums = nums.split(" ");
+		for(var i = 0; i < nums.length; i++){
+			nums[i] = parseInt(nums[i]);
+		}
+
+		$(".animado").empty();
+		$("#collapsibleOptima").collapsible("open", 0);
+		crearAnimDV(nums, 0 , nums.length - 1);
+		divideMSS(nums, 0, nums.length - 1, true, editOptima).then(function(resDV){
+			$("#res").text("Resultado: "+resDV);
+		});
+		
 	});
 });
 
@@ -96,24 +111,20 @@ function crearAnimFB(numeros){
 	$("#normal").append(parcial);
 }
 
-//Función que bloquea el hilo de ejecución actual porque es llamada con await
-function esperar(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function crearAnimDV(numeros){
-	for(var i = 0; i <= numeros.length; i++){
+function crearAnimDV(numeros, inicio, fin){
+	if(inicio != fin){
+		$("#optimizada").append("<br>");
+		var contenedor = document.createElement("contenedor");
+		contenedor.classList.add("cont");
+		contenedor.setAttribute("id","n"+nivel);
+		$("#optimizada").append(contenedor);
+	}	
+	for(var i = inicio; i <= fin; i++){
 		var espacioDV = document.createElement("div");
 		espacioDV.classList.add("espacio");
-		espacioDV.setAttribute("id","dv"+i);
-		if(numeros[i]){
-			espacioDV.innerText = numeros[i];
-		}
-		else{
-			espacioDV.innerText = "Resultado: 0";
-			espacioDV.classList.add("resultado");
-		}
-		$("#optimizada").append(espacioDV);
+		espacioDV.setAttribute("id","dv"+i+nivel);
+		espacioDV.innerText = numeros[i];
+		$("#n"+nivel).append(espacioDV);
 	}
 }
 
@@ -121,7 +132,7 @@ async function fuerzaBrutaMSS(arreglo, tam, animar, editNormal){
 	var aux = arreglo[0];
 	var cont;
 
-	animar && editNormal.gotoLine(2, 0, true);
+	animar && editNormal.gotoLine(3, 0, true);
 	animar && $("#fb"+tam).removeClass("resultado");
 	animar && $("#fb0").addClass("actual");
 	animar && $("#fb"+tam).text("Resultado: "+aux);
@@ -130,7 +141,7 @@ async function fuerzaBrutaMSS(arreglo, tam, animar, editNormal){
 	for (var i = 0; i < tam; i++){
 		animar && $("#fb"+(tam - 1)).removeClass("actual");
 		cont = 0;
-		animar && editNormal.gotoLine(4, 0, true);
+		animar && editNormal.gotoLine(5, 0, true);
 		animar && await esperar(1000);
 		animar && $("#parcialfb").text("Resultado actual: "+cont);
 		for (var j = i; j < tam; j++){
@@ -138,58 +149,120 @@ async function fuerzaBrutaMSS(arreglo, tam, animar, editNormal){
 			animar && $("#fb"+j).addClass("actual");
 			animar && $("#parcialfb").text("Resultado actual: "+cont+" + "+arreglo[j]);
 			cont = cont + arreglo[j];
-			animar && editNormal.gotoLine(6, 0, true);
+			animar && editNormal.gotoLine(7, 0, true);
 			animar && await esperar(1000);
 			animar && $("#parcialfb").text("Resultado actual: "+cont);
 			animar && await esperar(1000);
 			if (cont > aux){
 				aux = cont;
-				animar && editNormal.gotoLine(8, 0, true);
+				animar && editNormal.gotoLine(9, 0, true);
 				animar && $("#fb"+tam).addClass("resultado");
 				animar && $("#fb"+tam).text("Resultado: "+aux);
 				animar && await esperar(1000);
 				animar && $("#fb"+tam).removeClass("resultado");
 			}
-			animar && editNormal.gotoLine(9, 0, true);
+			animar && editNormal.gotoLine(10, 0, true);
 			animar && await esperar(1000);
 		}
 	}
 	animar && $("#fb"+(tam - 1)).removeClass("actual");
 	animar && $("#fb"+tam).addClass("resultado");
-	animar && editNormal.gotoLine(12, 0, true);
+	animar && editNormal.gotoLine(13, 0, true);
+	animar && $("#fb"+tam).text("Resultado final: "+aux);
 	return aux;
 }
 
-function divideMSS(arreglo, inicio, fin){
+async function divideMSS(arreglo, inicio, fin, animar, editOptima){
+	animar && editOptima.gotoLine(1, 0, true);
+	animar && await esperar(1000);
 	if(inicio == fin){
+		animar && editOptima.gotoLine(3, 0, true);
+		animar && await esperar(1000);
 		return arreglo[inicio];
 	}
+	animar && editOptima.gotoLine(5, 0, true);
+	animar && await esperar(1000);
 	var mitad = Math.trunc((inicio+fin)/2);
   
 	var sumaIzquierda = 0,
 		sumaDerecha = 0,
 		sumaCentral = 0,
 		suma;
-	var izq = divideMSS(arreglo, inicio, mitad);
-	var der = divideMSS(arreglo, mitad+1, fin);
+	animar && editOptima.gotoLine(11, 0, true);
+	animar && await esperar(1000);
+	if(animar === true){
+		++nivel;
+		animar && crearAnimDV(arreglo, inicio, mitad);
+		var izq = await divideMSS(arreglo, inicio, mitad, true, editOptima);
+	}else{
+		var izq = await divideMSS(arreglo, inicio, mitad);
+	}
+	
+	animar && editOptima.gotoLine(12, 0, true);
+	animar && await esperar(1000);
+	if(animar === true){
+		++nivel;
+		animar && crearAnimDV(arreglo, mitad+1, fin);
+		var der = await divideMSS(arreglo, mitad+1, fin, true, editOptima);
+	}else{
+		var der = await divideMSS(arreglo, mitad+1, fin);
+	}
+	
   
+	animar && editOptima.gotoLine(14, 0, true);
+	animar && await esperar(1000);
 	suma = 0;
+	animar && editOptima.gotoLine(15, 0, true);
+	animar && await esperar(1000);
 	sumaIzquierda = arreglo[mitad];
 	for (let i = mitad; i >= inicio ; i--){
+		animar && editOptima.gotoLine(17, 0, true);
+		animar && await esperar(1000);
 		suma += arreglo[i];
 		if(suma > sumaIzquierda){
+			animar && editOptima.gotoLine(19, 0, true);
+			animar && await esperar(1000);
 			sumaIzquierda = suma;
 		}
+		animar && editOptima.gotoLine(20, 0, true);
+		animar && await esperar(1000);
 	}
 
+	animar && editOptima.gotoLine(23, 0, true);
+	animar && await esperar(1000);
 	suma = 0;
+	animar && editOptima.gotoLine(24, 0, true);
+	animar && await esperar(1000);
 	sumaDerecha = arreglo[mitad + 1];
 	for (let i = mitad + 1; i <= fin; i++){
+		animar && editOptima.gotoLine(26, 0, true);
+		animar && await esperar(1000);
+		//animar && $("#dv"+i+nivel).addClass("actual");
 		suma += arreglo[i];
 		if(suma > sumaDerecha){
+			animar && editOptima.gotoLine(28, 0, true);
+			animar && await esperar(1000);
 			sumaDerecha = suma;
 		}
+		//animar && $("#dv"+i+nivel).removeClass("actual");
+		animar && editOptima.gotoLine(29, 0, true);
+		animar && await esperar(1000);
 	}
+
+	animar && editOptima.gotoLine(31, 0, true);
+	animar && await esperar(1000);
 	sumaCentral = sumaIzquierda + sumaDerecha;
-	return Math.max(izq, der, sumaCentral);
+	animar && editOptima.gotoLine(32, 0, true);
+	animar && await esperar(1000);
+
+	var valorMaximo = Math.max(izq, der, sumaCentral);
+	animar && $(".cont").last().remove();
+	animar && $("br").last().remove();
+	--nivel;
+	return valorMaximo;
+}
+
+//Función que bloquea el hilo de ejecución actual porque es llamada con await
+function esperar(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
